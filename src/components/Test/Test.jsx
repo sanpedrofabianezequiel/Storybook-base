bb89253b91c70dca0fc3b1c8819182cfb163dc7d
Fabian Ezequiel San Pedro <fabian.sanpedro@globant.com>
2022-04-30 00:01:07 -0300

    Components basic

---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,4 @@
+
+export default function Test(){
+    return <h1>Test</h1>
+}
---
+++ src/components/Test/Test.stories.jsx
@@ -0,0 +1,8 @@
+
+export default {
+    title:'Components/Test',
+    component:Button,
+    argTypes:{
+        backgroundColor:{control:'color'},
+    }
+}
